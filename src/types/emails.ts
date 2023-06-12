0a51b151609daf812c6c0fc80e07b64d7681e27f
remorses <beats.by.morse@gmail.com>
2023-06-12 12:01:07 +0200

    added param

--- src/types/emails.ts
+++ src/types/emails.ts
@@ -1,8 +1,9 @@
 export interface SendParams {
   to: string;
   subject: string;
+  type?: "markdown";
   body: string;
   from?: string;
-  name?:string;
+  name?: string;
   withUnsubscribe?: boolean;
 }
